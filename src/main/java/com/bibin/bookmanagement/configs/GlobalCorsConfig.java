26705c9f5544f3b4c021c5ad6c65e728b11be875
bibint6@gmail.com <bibint6@gmail.com>
2024-12-12 21:10:08 +0530

    Initial Commit

--- src/main/java/com/bibin/bookmanagement/configs/GlobalCorsConfig.java
+++ src/main/java/com/bibin/bookmanagement/configs/GlobalCorsConfig.java
@@ -1,5 +1,6 @@
 package com.bibin.bookmanagement.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,11 +10,14 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class GlobalCorsConfig {
 
+    @Value(value = "${cors.uri}")
+    private String corsUri;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // Allow credentials (e.g., cookies)
-        config.addAllowedOrigin("http://localhost:3000"); // Allow specific origin
+        config.addAllowedOrigin(corsUri); // Allow specific origin
         config.addAllowedHeader("*"); // Allow all headers
         config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
 
